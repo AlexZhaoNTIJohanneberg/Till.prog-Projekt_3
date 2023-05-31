601db170be6f2198e0f41615574be7e60203f80c
AlexZhaoNTIJohanneberg <119052322+AlexZhaoNTIJohanneberg@users.noreply.github.com>
2023-05-31 14:21:43 +0200

    Update Projekt_3.ino

--- Projekt_3/Projekt_3.ino
+++ Projekt_3/Projekt_3.ino
@@ -38,9 +38,9 @@ int leng = 0;
 void setup() {
 
   Serial.begin(9600);
-  LedSign::Init(GRAYSCALE);
+  LedSign::Init(GRAYSCALE);      // startar shield-LOL(LED skärmen)
 
-  for (int i = 0; ; i++) { //get the length of the text
+  for (int i = 0; ; i++) {
     if (text[i] == 0) {
       leng = i;
       break;
@@ -49,29 +49,30 @@ void setup() {
 
 }
 
-void setupgame() {
+void setupgame() {   //Uppstartning för spelet(genererar values)
 
-  gamespeed = 100;
+  gamespeed = 100;  //Hur snabbt spelet går(bl.a ormens hastighet)
 
-  snakeLength = 4;
-  snakeX[0] = 0;
-  snakeY[0] = random(0, 9);
-  dirX = 1;
-  dirY = 0;
+  alive = true;
+  snakeLength = 4;                 //Längden på ormen vid start
+  snakeX[0] = 0;                   //Startpunkten för ormen hos x-axeln
+  snakeY[0] = random(0, 9);        //Startpunkten för ormen hos y-axeln
+  dirX = 1;                        //Startriktning för ormen hos x-axeln
+  dirY = 0;                        //Startriktning för ormen hos y-axeln
 
-  foodX = random(0, 14);
-  foodY = random(0, 9);
+  foodX = random(0, 14);           //Genererar en slumpmässig x-koordinat för maten
+  foodY = random(0, 9);            //Genererar en slumpmässig y-koordinat för maten
 }
 
-void joystickinput() {
+void joystickinput() {  //Funktion som behandlar joysticken
 
-  joystickX = analogRead(VRx);
-  joystickY = analogRead(VRy);
+  joystickX = analogRead(VRx);   //Läser av pinout 'VRx' på joysticken som är den "vågräta" axeln och anger ett värde till joystickX
+  joystickY = analogRead(VRy);   //Läser av pinout 'VRy' på joysticken som är den "lodräta" axeln och anger ett värde till joystickY
 
 
-  if (dirX == 1) {
-    joystickY = analogRead(VRy);
-    if (joystickY < 300) {
+  if (dirX == 1) {                
+    joystickY = analogRead(VRy);  //Läser av pinout 'VRy' på joysticken som är den "lodräta" axeln och anger ett värde till joystickY
+    if (joystickY < 300) {        //If-sats som bestämmer riktning på y-axeln och sätter dirX till noll
       dirY = 1;
       dirX = 0;
     }
@@ -81,9 +82,9 @@ void joystickinput() {
     }
   }
 
-  if (dirX == -1) {
-    joystickY = analogRead(VRy);
-    if (joystickY < 300) {
+  if (dirX == -1) {               
+    joystickY = analogRead(VRy);  //Läser av pinout 'VRy' på joysticken som är den "lodräta" axeln och anger ett värde till joystickY
+    if (joystickY < 300) {        //If-sats som bestämmer riktning på y-axeln och sätter dirX till noll
       dirY = 1;
       dirX = 0;
     }
@@ -93,9 +94,9 @@ void joystickinput() {
     }
   }
 
-  if (dirY == 1) {
-    joystickX = analogRead(VRx);
-    if (joystickX < 300) {
+  if (dirY == 1) {                
+    joystickX = analogRead(VRx);  //Läser av pinout 'VRx' på joysticken som är den "vågräta" axeln och anger ett värde till joystickX
+    if (joystickX < 300) {        //If-sats som bestämmer riktning på x-axeln och sätter dirY till noll
       dirX = 1;
       dirY = 0;
     }
@@ -105,9 +106,9 @@ void joystickinput() {
     }
   }
 
-  if (dirY == -1) {
-    joystickX = analogRead(VRx);
-    if (joystickX < 300) {
+  if (dirY == -1) {               
+    joystickX = analogRead(VRx);  //Läser av pinout 'VRx' på joysticken som är den "vågräta" axeln och anger ett värde till joystickX
+    if (joystickX < 300) {        //If-sats som bestämmer riktning på x-axeln och sätter dirY till noll
       dirX = 1;
       dirY = 0;
     }
@@ -118,12 +119,12 @@ void joystickinput() {
   }
 }
 
-bool checkcollisionsnake() {
-  if (snakeX[0] < 0 || snakeX[0] >= GRIDSIZEX || snakeY[0] < 0 || snakeY[0] >= GRIDSIZEY) {
+bool checkcollisionsnake() {      //Funktion som returnerar en boolean. Funktionen kollar om ormen har krockat in i en vägg eller sig själv.
+  if (snakeX[0] < 0 || snakeX[0] >= GRIDSIZEX || snakeY[0] < 0 || snakeY[0] >= GRIDSIZEY) {    //Krocka in i en vägg
     return true;
   }
 
-  for (int i = 1; i < snakeLength; i++) {
+  for (int i = 1; i < snakeLength; i++) {         //Krocka in i sig själv
     if (snakeX[0] == snakeX[i] && snakeY[0] == snakeY[i]) {
       return true;
     }
@@ -131,46 +132,36 @@ bool checkcollisionsnake() {
   return false;
 }
 
-bool checkcollisionfood() {
+bool checkcollisionfood() {      //Funktion som returnerar en boolean. Funktionen kollar om ormen har krockat in i maten.
   return (snakeX[0] == foodX && snakeY[0] == foodY);
 }
 
-void drawfood() {
+void drawfood() {     //Funktion som lyser upp LED-dioden där maten befinner sig
   LedSign::Set(foodX, foodY, 4);
 }
 
-void drawsnake() {
-  for (int i = 0; i < snakeLength; i++) {
-    LedSign::Set(snakeX[i], snakeY[i], 2);
+void drawsnake() {     //Funktion som lyser upp ormen.
+  for (int i = 0; i < snakeLength; i++) {    //En for-loop med längden av ormenslängd som går igenom varje x- och y-värde ormen har.
+    LedSign::Set(snakeX[i], snakeY[i], 2);   //Anger hur starkt LED-dioden ska lysa på denna koordinat på ormen.
   }
 }
 
-void movesnake() {
+void movesnake() {     //Funktionen gör så att ormen rör på sig
 
   for (int i = snakeLength - 1; i > 0; i--) {
+    Serial.print("X before: ");
+    Serial.print(snakeX[i]);
+    Serial.print("\t");
     snakeX[i] = snakeX[i - 1];
-    snakeY[i] = snakeY[i - 1];
+    //snakeY[i] = snakeY[i - 1];
+    Serial.print("X after: ");
+    Serial.println(snakeX[i]);
+
+    
   }
 
   snakeX[0] += dirX;
   snakeY[0] += dirY;
-
-
-  // Kan användas så att man inte dör om man kör in i 'väggen'. I detta fall
-  // används denna del kod för att "snygga" till när man kör in i väggen.
-  if (snakeX[0] < 0) {
-    snakeX[0] = GRIDSIZEX + 1;
-  }
-  else if (snakeX[0] >= GRIDSIZEX) {
-    snakeX[0] = -1;
-  }
-
-  if (snakeY[0] < -1) {
-    snakeY[0] = GRIDSIZEY + 1;
-  }
-  else if (snakeY[0] > GRIDSIZEY) {
-    snakeY[0] = -1;
-  }
 
 }
 
@@ -201,9 +192,8 @@ void snakegame() {
 
 void loop() {
   setupgame();
-  alive = true;
 
-  Myfont::Banner(leng, text);
+  //Myfont::Banner(leng, text);
 
   if (analogRead(VRb) == 0) {
     start = true;
@@ -216,6 +206,7 @@ void loop() {
     snakegame();
   }
   LedSign::Clear();
+  
   for (int i = 0; i < snakeLength; i++) {
     snakeX[i] = -1;
     snakeY[i] = -1;
